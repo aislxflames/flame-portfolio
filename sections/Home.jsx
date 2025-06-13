566aa6aecd69bed733e934496004b5f62cd5a161
aislxflames <proarghaop@gmail.com>
2025-06-13 23:46:55 +0530

    Ui fixes

--- sections/Home.jsx
+++ sections/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <section id="home" className={`flex md:flex-row flex-col md:justify-around justify-center md:px-20 sm:px-10 px-5 font-montserrat h-[100vh] ${stylesbg.anibg} items-center overflow-hidden`}>
       <div className="left relative flex flex-col gap-5">
         <div className='z-10 flex flex-col gap-5 md:items-start items-center'>
-          <h1 className="font-extrabold md:text-6xl text-6xl">
+          <h1 className="font-extrabold md:text-6xl text-5xl text-start w-[90%]">
             <TypeAnimation
               sequence={[
                 'Frontend',
@@ -28,7 +28,7 @@ const Home = () => {
             </div>
           </h1>
 
-          <p className="text-wrap md:w-[60%] w-[90%] md:block text-[0.9rem] text-gray-300">
+          <p className="text-wrap md:w-[60%] w-[90%] md:block md:text-sm text-[0.8rem] text-gray-300">
             Hey I am Aislx, a Fullstack Developer with a passion for creating dynamic and responsive web applications. I specialize in building scalable and efficient solutions using modern technologies like Next.js, JavaScript, MongoDB, and Python. Let's bring your ideas to life!
           </p>
 
@@ -60,7 +60,7 @@ const Home = () => {
       </div>
 
       <div className="right relative md:block flex flex-col justify-center items-center">
-        <img className={`${styles.float} w-[40vw] md:w-[60vw]`} src="/computer.png" alt="" />
+        <img className={`${styles.float} w-[50vw] md:w-[60vw]`} src="/computer.png" alt="" />
         <div className='rounded-xl overflow-hidden opacity-60 bg-black/10 backdrop-blur-2xl'>
           <Spotify wide className='z-10 b-10' link="https://open.spotify.com/track/23Jw6HlYfuhyllJhm3WPSc?si=9539c6b08d0a42f7" />
         </div>
